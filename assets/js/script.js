66b4b88cc4911f060c6b7208a7c999a99712fec7
Sharon Abramson <sharabramson@gmail.com>
2023-11-21 09:48:31 -0500

    created checkAnswer function, added querySelectors for buttons, formatted index.html, styled buttons and formatted webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,50 @@
-var startBtn = document.querySelector("#startQuiz");
-var timerEl = document.getElementById('timer');
-var viewDisplay = document.querySelector("#viewDisplay");
+let startBtn = document.querySelector("#startQuiz");
+let timerEl = document.getElementById('timer');
+let introSection = document.querySelector("#introSection");
+let questionSection = document.querySelector("#questionSection");
+let gameOverSection = document.querySelector("#gameOverSection");
+let highscoresSection = document.querySelector("#higscoresSection");
+let submitBtn = document.querySelector("#submitBtn");
+let questionBtn = document.querySelector(".questionBtn");
 
-// var questions [];
+// let questionNumber = 0;
+// let answerNumber = 0;
+let isRight = false;
+let chosenAnswer = false;
+let timeLeft = 0;
 
-//Add event listener to the start quiz button
-startBtn.addEventListener("click", startQuiz);
-// answerBtn.addEventListener("click", answer);
-// startBtn.addEventListener("click", startQuiz);
-// startBtn.addEventListener("click", startQuiz);
+let correctAnswers = [];
 
-quizContent = {
+let quizContent = {
   questions: ["Commonly used data types DO NOT include: ", 
               "The condition in an if/else statement is enclosed within ______.", 
               "Arrays in JavaScript can be used to store______.", 
-              "String values must be enclosed within ______ when being assigned to variables.", 
+              "String values must be enclosed within ______ when being assigned to letiables.", 
               "A very useful tool used during development and debugging for printing content to the debugger is:"],
-  answers: [
+  choices: [
     ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
     ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
     ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
     ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
     ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"]
   ],
-  currentQuestion: 0,
+  correctAnswers: [2, 1, 3, 2, 3],
+  currentQuestionIndex: 0,
 }
+//Add event listener to the start quiz button
+startBtn.addEventListener("click", startQuiz);
+
+
+answerBtn.addEventListener("click", checkAnswer);
+
+// startBtn.addEventListener("click", clearHighscores);
+// startBtn.addEventListener("click", playAgain);
+
+
 
 function startTimer(){ 
-  var timeLeft = 60;
+  console.log("start");
+  timeLeft = 60;
   let timeInterval = setInterval(function () {
     if(timeLeft > 1) {
       timerEl.textContent = timeLeft;
@@ -42,28 +59,54 @@ function startTimer(){
 function startQuiz() {
   // console.log("start");
   startTimer();
+  introSection.setAttribute("style", "display: none;")
   questionPresent();
+  questionSection.setAttribute("style", "display: block;")
 
+  
 }
 
+
 function questionPresent() {
-  viewDisplay.innerHTML = "";
+  let question = document.querySelector(".question");
+  question.textContent = quizContent.questions[quizContent.currentQuestionIndex];
 
-  let h1 = document.createElement("h1");
-  h1.textContent = "question";
-  h1.setAttribute("data-index", 0); //change 0 to i
- 
-  let ul = document.createElement("ul");
+  let questionBtn = document.querySelectorAll(".questionBtn");
+  console.log(quizContent.choices[quizContent.currentQuestionIndex].length);
+  for (let i = 0; i < quizContent.choices[quizContent.currentQuestionIndex].length; i++) {
+    questionBtn[i].textContent = quizContent.choices[quizContent.currentQuestionIndex][i];
+    questionBtn[i].addEventListener("click", checkAnswer);
+    // console.log(questionBtn);
+  }
+}
 
-  let li = document.createElement("li");
-  li.textContent = "answers";
-  //create button
-  li.setAttribute("data-index", 0); //change 0 to i
-  ul.appendChild(li);
-  //for loop
+function gameOver() {
+  questionSection.setAttribute("style", "display: none;")
+  gameOverSection.setAttribute("style", "display: block;")
 
-  viewDisplay.appendChild(h1);
-  viewDisplay.appendChild(ul);
   
+  submitBtn.addEventListener("click", showHighscores);
+}
 
+function showHighscores() {
+  let scores = localStorage.getItem("scores");
+  scoresList.textContent = scores;
+}
+
+function checkAnswer(evt) {
+  let currentChoiceIndex = evt.currentTarget.getAttribute("value");
+  console.log("This is the ID: " + currentChoiceIndex);
+  if (quizContent.correctAnswers[quizContent.currentQuestionIndex] == currentChoiceIndex) {
+    isWin = true;
+  } else {
+    timeLeft-=5;
+  }
+  
+  console.log("check answer");
+  if (quizContent.currentQuestionIndex >= 4 || timeLeft == 0) {
+    gameOver();
+  } else {
+    quizContent.currentQuestionIndex++;
+    questionPresent();
+  }
 }
